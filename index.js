3cfbc80034ad07373fbbc4a6fc80a9d3c7c9c40f
Nataliya Rodikova <bachininang@gmail.com>
2024-12-13 13:29:20 +0500

    counter

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const app = () => {
+  let counterValue = 0;
+
   const result = document.getElementById('result');
 
+  const incHandler = () => {
+    counterValue += 1;
+    result.textContent = counterValue;
+  }
+
+  const decHandler = () => {
+    counterValue -= 1;
+    result.textContent = counterValue;
+  }
+
   const inc = document.getElementById('increment');
-  inc.addEventListener('click', () => {
-    result.textContent = parseInt(result.textContent, 10) + 1;
-  });
+  inc.addEventListener('click', incHandler);
+
+  const dec = document.getElementById('decrement');
+  dec.addEventListener('click', decHandler);
+  
 };
 
 app();
