3caf9f66ed33b9c2b310e0e951f4f04a09e38cc1
Nataliya Rodikova <bachininang@gmail.com>
2025-01-16 22:09:54 +0500

    state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-import 'core-js/stable/index.js';
-import 'regenerator-runtime/runtime.js';
-import app from './application.js';
+const render = (state, formEl, inputEl, resultEl) => {
+  formEl.reset();
+  inputEl.focus();
+  resultEl.textContent = state;
+};
+
+const app = () => {
+  // состояние относится к уровню приложения
+  let state = 0;
+
+  const formEl = document.querySelector('form');
+  const inputEl = document.querySelector('input[type=number]');
+  const resetEl = document.querySelector('button');
+  const resultEl = document.querySelector('#result');
+
+  formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    state += parseInt(data.get('number'), 10);
+    render(state, formEl, inputEl, resultEl);
+  });
+
+  resetEl.addEventListener('click', () => {
+    state = 0;
+    render(state, formEl, inputEl, resultEl);
+  });
+
+  inputEl.focus();
+};
 
 app();
